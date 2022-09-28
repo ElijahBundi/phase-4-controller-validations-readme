5ef813d7079d93ee8d2ce088a24e656ae5e09894
elijahbundi <bundielly@gmail.com>
2022-09-28 22:07:48 +0300

    assignment complete

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,2 +1,63 @@
 class Bird < ApplicationRecord
+    # validates :name, presence: true, uniqueness: true
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    # GET /birds
+    def index
+      birds = Bird.all
+      render json: birds
+    end
+  
+    # POST /birds
+    def create
+      bird = Bird.create!(bird_params)
+      render json: bird, status: :created
+    end
+  
+    # GET /birds/:id
+    def show
+      bird = find_bird
+      render json: bird
+    end
+  
+    # PATCH /birds/:id
+    def update
+      bird = find_bird
+      bird.update!(bird_params)
+      render json: bird
+    end
+  
+    # PATCH /birds/:id/like
+    def increment_likes
+      bird = find_bird
+      bird.update(likes: bird.likes + 1)
+      render json: bird
+    end
+  
+    # DELETE /birds/:id
+    def destroy
+      bird = find_bird
+      bird.destroy
+      head :no_content
+    end
+  
+    private
+  
+    def find_bird
+      Bird.find(params[:id])
+    end
+  
+    def bird_params
+      params.permit(:name, :species, :likes)
+    end
+  
+    def render_not_found_response
+      render json: {error: "Bird not found"}, status: :not_found
+    end
+
+    def render_unprocessable_entity_response
+        render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+    end
+
 end
